97230f55bbc1f15837f3ea85e6f1f972a98cfd91
Aradhya Nepal <aradhya.1221@gmail.com>
2024-02-02 20:10:23 +0545

    Final Extra improvements

--- lib/textfield.dart
+++ lib/textfield.dart
@@ -12,7 +12,7 @@ class AutocompleteBasicExample extends StatefulWidget {
 
 class _AutocompleteBasicExampleState extends State<AutocompleteBasicExample> {
   final _suggestionOpenedController = ValueNotifier(false);
-  final _globalKey=GlobalKey();
+  final _globalKey = GlobalKey();
 
   @override
   void dispose() {
@@ -28,29 +28,32 @@ class _AutocompleteBasicExampleState extends State<AutocompleteBasicExample> {
           return Align(
             alignment: Alignment.topLeft,
             child: Material(
-                child: Container(
-                    clipBehavior: Clip.hardEdge,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.only(
-                        bottomLeft: Radius.circular(10.r),
-                        bottomRight: Radius.circular(10.r),
-                      ),
-                      boxShadow: [
-                        BoxShadow(
-                          blurRadius: 4.r,
-                          offset: Offset(0, 4.h),
-                          color: const Color(0x10000000),
-                        )
-                      ],
-                      color: Colors.white,
-                    ),
-                    width: constraints.biggest.width,
-                    child: SingleChildScrollView(
-                        child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: options.map((opt) {
-                        return InkWell(//Todo: DO NOT USE GESTUREDETECTOR HERE, BAD TOUCHING EXPERIENCE TO THE USER
-                          splashColor: Colors.transparent,
+              child: Container(
+                clipBehavior: Clip.hardEdge,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(10.r),
+                    bottomRight: Radius.circular(10.r),
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                      blurRadius: 4.r,
+                      offset: Offset(0, 4.h),
+                      color: const Color(0x10000000),
+                    )
+                  ],
+                  color: Colors.white,
+                ),
+                width: constraints.biggest.width,
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: options.map(
+                      (opt) {
+                        return InkWell(
+                            //Todo: DO NOT USE GESTURE DETECTOR HERE, BAD TOUCHING EXPERIENCE TO THE USER.
+                            //Todo: i.e USER IS ONLY ALLOWED TO CLICK THE TEXT WITH PRECISION, NOT THE RIGHT EMPTY SPACE
+                            splashColor: Colors.transparent,
                             onTap: () {
                               onSelected(opt);
                             },
@@ -58,27 +61,20 @@ class _AutocompleteBasicExampleState extends State<AutocompleteBasicExample> {
                               padding: EdgeInsets.all(6.h),
                               child: Text(
                                 opt,
-                                // style: generalMontserratTextStyle(16)
+                                // style: generalMontserratTextStyle(16) //TODO: IMPLEMENT IT
                                 //     .copyWith(fontWeight: FontWeight.w500),
                               ),
                             ));
-                      }).toList(),
-                    )))),
+                      },
+                    ).toList(),
+                  ),
+                ),
+              ),
+            ),
           );
         },
         fieldViewBuilder:
             (context, textEditingController, focusNode, onFieldSubmitted) {
-
-          // debugger();
-
-          // focusNode.addListener(() {
-          //   if (focusNode.hasFocus !=
-          //       _suggestionOpenedController.value) {
-          //     // debugger();
-          //     _suggestionOpenedController.value=focusNode.hasFocus;
-          //     // debugger();
-          //   }
-          // });
           return ValueListenableBuilder(
             valueListenable: _suggestionOpenedController,
             builder: (context, value, child) {
@@ -99,11 +95,12 @@ class _AutocompleteBasicExampleState extends State<AutocompleteBasicExample> {
                 child: child ?? const SizedBox(),
               );
             },
-            child: Focus(//Just better way to Listen, in addListener we need to remove Listener too, else it might cause memory leaks
-              onFocusChange: (hasFocus){
-                //No need to check whether the value is checked
+            child: Focus(
+              //Just better way to Listen, in addListener we need to remove Listener too, else it might cause memory leaks
+              onFocusChange: (hasFocus) {
+                //No need to check whether the value changed
                 //Value notifier is smart enough to do it auto
-                _suggestionOpenedController.value=focusNode.hasFocus;
+                _suggestionOpenedController.value = focusNode.hasFocus;
               },
               child: TextFormField(
                 key: _globalKey,
@@ -115,17 +112,21 @@ class _AutocompleteBasicExampleState extends State<AutocompleteBasicExample> {
           );
         },
         optionsBuilder: (TextEditingValue textEditingValue) {
-          final output= kCountries.where((element) => element
+          final output = kCountries.where((element) => element
               .toLowerCase()
               .contains(textEditingValue.text.toLowerCase()));
+          //No need to check whether the value changed
+          //Value notifier is smart enough to do it auto
+          _suggestionOpenedController.value = output
+              .isNotEmpty; //TODO: VERIFY WHETHER THIS EXTRA FEATURE IS HELPFUL, i.e ON NO ITEM, DECORATION GETS REMOVED
           return output;
         },
         onSelected: (String selection) {
           // If you decided to not UnFocus on selected,
-          // then you should remove below code too else will cause UI mismatch
-          // i.e: on item selected background changes,
-          // but when you clear item and re-search the dropdown items will show
-          // but the background will not change
+          // then you should remove code below it too.
+          // ELSE UI mismatch
+          // On item selected background changes, but when you clear item and re-search:
+          // the dropdown items will show but the background will not change. <-Bug
           // FocusScope.of(context).unfocus();
           // _suggestionOpenedController.value = false;
         },
